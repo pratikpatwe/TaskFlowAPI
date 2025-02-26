e0230633846c551fd3c37b3f9d67e5ba17382010
pratikpatwe <pratikpatwe79@gmail.com>
2025-02-26 05:48:57 +0530

    created routes folder and created one rout for users in that now I'm going to bed

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express();
 
 import {PORT,SERVER_URL} from './config/env.js'
 
+//routes
+import userRouter from './routes/users.route.js';
+
 app.get('/', (req,res) => {
   res.send('<body style="display:flex; justify-content:center; align-items:center; background-color:#222;"><div style="display:flex; flex-direction:column; justify-content:center; height:200px; width:500px; color:#fff; background-color:#111; border-radius:15px; padding:20px;"><h1>TaskFlowAPI</h1><p>TaskFlowAPI is a backend API for managing tasks with user authentication, CRUD operations, task statuses, due dates, filtering, and sorting. It ensures secure, efficient task tracking with role-based access.</p></div></body>');
 });
+
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on : ${SERVER_URL}`);
